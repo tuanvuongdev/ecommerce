fe7b31bd20cfa79b9bf1dfaa37fea7212f9fd010
vuong.pham <phamtuanvuong.dev@gmail.com>
2025-04-21 19:40:22 +0700

    add discord config

---
+++ src/loggers/discord.log.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged is as ${client.user.tag}`);
+});
